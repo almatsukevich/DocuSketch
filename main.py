2ef809bf486cb5965eff7612abf271edc9278bc7
Alexander Matsukevich <al.matsukevich@yandex.by>
2021-10-22 11:05:31 +0300

    Fifth commit

--- main.py
+++ main.py
@@ -7,8 +7,10 @@ class DrawPlots:
         dfs = df.sort_values('mean', ascending=False) #sort DataFrame by value "mean"
         dfs.plot(x="name", y=["mean", "max", "min"])
         plt.savefig('plots/plot_mean.png')
+        dfs = df.sort_values('floor_mean', ascending=False) #sort DataFrame by value "mean"
         dfs.plot(x="name", y=["floor_mean", "floor_max", "floor_min"])
         plt.savefig('plots/plot_floor.png')
+        dfs = df.sort_values('ceiling_mean', ascending=False) #sort DataFrame by value "mean"
         dfs.plot(x="name", y=["ceiling_mean", "ceiling_max", "ceiling_min"])
         plt.savefig('plots/plot_ceiling.png')
         return plt.show()
