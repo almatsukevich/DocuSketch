64cd4373bfe482b63c3c4d62ae435874a445ca8c
Alexander Matsukevich <al.matsukevich@yandex.by>
2021-10-22 10:22:06 +0300

    Third commit

--- main.py
+++ main.py
@@ -3,9 +3,15 @@ import matplotlib.pyplot as plt
 class DrawPlots:
     
     def draw_plots(self):
-        df = pd.read_json('deviation.json')
-        t=plt.plot(df['mean'], df['max'], df['min'])
-        return print(t)
+        df = pd.read_json('deviation.json') #reading file
+        dfs = df.sort_values('mean', ascending=False) #sort DataFrame by value "mean"
+        dfs.plot(x="name", y=["mean", "max", "min"])
+        plt.savefig('plots/plot_mean.png')
+        dfs.plot(x="name", y=["floor_mean", "floor_max", "floor_min"])
+        plt.savefig('plots/plot_floor.png')
+        dfs.plot(x="name", y=["ceiling_mean", "ceiling_max", "ceiling_min"])
+        plt.savefig('plots/plot_ceiling.png')
+        return plt.show()
 
 g = DrawPlots()
 g.draw_plots()
